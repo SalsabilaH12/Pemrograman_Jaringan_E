a51d5daad1326144b6abffcb9937f04584103830
SalsabilaH12 <harlensalsabila@gmail.com>
2021-07-18 17:06:02 +0700

    Praktikum Implementasi Kasus Concurrency

---
+++ progjar3/Tugas_3 Implementasi Kasus Concurrency/server1.py
@@ -0,0 +1,16 @@
+import socket
+
+SERVER_IP = '192.168.122.130'
+SERVER_PORT = 5758
+
+serverSock = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+serverSock.bind(((UDP_IP, UDP_PORT)))
+filename='server1.jpg'
+fp = open(filename,'wb+')
+ditulis=0
+count=0
+while True:
+    data, addr = serverSock.recvfrom(1024)
+    count=count+len(data)
+    print(addr, count, len(data), data)
+    fp.write(data)
